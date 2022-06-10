678ad3254604090e08f03488067b78914d706770
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-06-10 12:41:17 +0530

    Time: 49 ms (24.41%), Space: 9 MB (38.67%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,29 +5,18 @@ public:
         if(s.empty()) return 0;
         unordered_map<char, int> mp;
         int f=0, e=0;
-        int n=s.length(), cnt=0, maxi=0;
+        int n=s.length(), maxi=0;
         
         while(e<n)
         {
-            if(mp.find(s[e]) == mp.end())
-            {
-                mp[s[e]] = e;
-                cout<<s[e]<<" "<<e<<endl;
-                cnt = e-f+1;
-                maxi = max(maxi, cnt); 
-                e++;
-            }
-            
-            else
+            if(mp.find(s[e]) != mp.end())
             {
                 while(f<mp[s[e]]) mp.erase(s[f++]);
                 f = mp[s[e]]+1;
-                mp[s[e]] = e;
-                cout<<s[e]<<" "<<e<<endl;
-                cnt = e-f+1;
-                maxi = max(maxi, cnt); 
-                e++;
             }
+            mp[s[e]] = e;
+            maxi = max(maxi, e-f+1);
+            e++;
         }
         return maxi;
         
