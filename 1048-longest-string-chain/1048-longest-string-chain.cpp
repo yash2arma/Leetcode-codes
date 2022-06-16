c10c15f258c34eb83db58c94cedad2ee408ed6ba
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-06-16 23:31:27 +0530

    Time: 170 ms (38.54%), Space: 27.2 MB (20.62%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -11,7 +11,6 @@
 
 class Solution {
 public:
-    
     static bool compare(string s1, string s2)
     {
         return s1.size()<s2.size();
@@ -19,30 +18,27 @@ public:
     
     int longestStrChain(vector<string>& words) 
     {
-        unordered_map<string, int> dp; //define hashmap dp which stores predecessor word and chain size for it
+        unordered_map<string, int> dp; //it stores predecessor word and chain size
         
         //sort(words.begin(), words.end(), [](const string &s1, const string &s2){ return s1.length()<s2.length();});
         sort(words.begin(), words.end(), compare);
         
-        int len = 1; //store longest possible word chain
-        
-        for(auto word:words) //cover each word in the vector words 
+        int len = 1; 
+        for(auto word:words) 
         {
-            dp[word]=1; //A single word has word chain 1
-            for(int i=0; i<word.length(); i++) //cover all the alphabets in the word
+            dp[word]=1; 
+            for(int i=0; i<word.length(); i++) 
             {
                 //removing ith alphabet from the string and check remaining string is available in the dp 
                 string pred = word.substr(0,i) + word.substr(i+1); 
                 
-                if(dp.find(pred) != dp.end()) //if it presents
+                if(dp.find(pred) != dp.end()) 
                 {
-                    dp[word] = dp[pred]+1; //then increase word chain by 1 and store in dp
-                    len = max(len, dp[word]); //find max length
+                    dp[word] = max(dp[word], dp[pred]+1); 
+                    len = max(len, dp[word]);
                 }
             }
         }
-        return len;
-        
-        
+        return len;    
     }
 };
