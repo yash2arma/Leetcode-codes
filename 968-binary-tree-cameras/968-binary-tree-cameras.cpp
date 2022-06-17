e159c4d91a1d793c567d2c3873abcff6cc20cab9
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-06-17 14:02:13 +0530

    Time: 8 ms (90.78%), Space: 21.1 MB (87.57%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -9,14 +9,24 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+/*Approach: We will start installing cameras from the bottom and go up to the root node.
+we are gonna use couple of variables 1, -1, 0
+where:
+1 -> This node don't want camera
+-1 -> this node needs camera to be installed
+0 -> camera is installed here
+
+the intuition is we will reach to the leaf node and install camera to the parent of leaf so that it can cover max 2 leaf nodes.*/
+    
+/*class Solution {
 public:
     int minCameraCover(TreeNode* root) 
     {
         int cam = 0;
         int miniCam = cover(root, cam);
         
-        if(miniCam==-1)
+        if(miniCam==-1) //need to install a cam here
             cam++;
         
         return cam;
@@ -24,21 +34,64 @@ public:
     
     int cover(TreeNode* root, int &cam)
     {
-        if(root==NULL) return 1;
+        if(root==NULL) return 1; //This is not a node so it doesn't need a cam
         
-        int left = cover(root->left, cam);
+        //we go at the bottom of the tree i.e. at leaf nodes
+        int left = cover(root->left, cam); 
         int right = cover(root->right, cam);
         
-        if(left==-1 || right==-1)
+        //While returning we check which node need cam
+        if(left==-1 || right==-1) //Node's child want cam so we install cam here 
         {
             cam++;
             return 0;
         }
         
-        else if(left==0 || right==0)
+        else if(left==0 || right==0) //Cam installed at node's child, so node doesn't need a cam
             return 1;
         
+        //for (left==1 || right==1) condition, children doesn't need a cam but parent have
         return -1;
+    }
+        
+};
+*/
+//0 - need cam
+//1 - doesn't need
+//2 - cam install
+class Solution {
+public:
+    int minCameraCover(TreeNode* root) 
+    {
+        int cam = 0;
+        int miniCam = cover(root, cam);
+        
+        if(miniCam==0) //need to install a cam here
+            cam++;
+        
+        return cam;
+    }
+    
+    int cover(TreeNode* root, int &cam)
+    {
+        if(root==NULL) return 1; //This is not a node so it doesn't need a cam
+        
+        //we go at the bottom of the tree i.e. at leaf nodes
+        int left = cover(root->left, cam); 
+        int right = cover(root->right, cam);
+        
+        //While returning to the top we check which node needs cam
+        if(left==0 || right==0) //Node's child want cam so we install cam here 
+        {
+            cam++;
+            return 2;
+        }
+        
+        else if(left==2 || right==2) //Cam installed at node's child, so node doesn't need a cam
+            return 1;
+        
+        //for (left==1 || right==1) condition, children doesn't need a cam but parents have
+        return 0; 
     }
         
 };
