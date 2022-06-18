856d0d0a6d98407a40360ff2428e9205b47e6019
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-06-18 14:38:21 +0530

    Time: 1255 ms (9.71%), Space: 150.1 MB (63.81%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,22 +1,26 @@
 class WordFilter {
 public:
-    unordered_map<string, int> mp;
+    unordered_map<string, int> mp; //stores preffix and suffix sequence in it
     
     WordFilter(vector<string>& words) 
     {
         int n = words.size();
         
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n; i++) //cover each word in words
         {
-            string word = words[i];
+            string word = words[i]; 
             int wordsize = word.size();
-            for(int j=1; j<=wordsize; j++)
+            
+            //storing all possible sequence of prefix and suffix of a word in the hashmap
+            for(int j=1; j<=wordsize; j++) //for preffix 
             {
-                string p = word.substr(0,j);
-                for(int k=0; k<wordsize; k++)
+                string pre = word.substr(0,j);
+                
+                for(int k=0; k<wordsize; k++) //for suffix
                 {
-                    string s = word.substr(k, wordsize);
-                    mp[p +"|"+ s] = i+1;
+                    string suff = word.substr(k, wordsize);
+                    mp[pre + "|" + suff] = i+1; //set index i+1 for all possible sequence of a word
+                    //we set i+1 to handle i=0 because by-default map initialized with 0 
                 }
             }
         }
@@ -25,7 +29,7 @@ public:
     int f(string prefix, string suffix) 
     {
         string s = prefix + "|" + suffix;
-        return mp[s]-1;
+        return mp[s]-1; //if sequence found in hashmap we return its index
     }
 };
 
