92e8e4110adeac38d1b23a0cf3962ac7b087e2b2
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-06-20 10:57:09 +0530

    Time: 30 ms (100.00%), Space: 13.8 MB (96.04%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    
-    static bool larger(string &s1, string &s2)
-    {
-        return s1.length() > s2.length();
-    }
-    int minimumLengthEncoding(vector<string>& words) 
-    {
-        unordered_map<string, int> mp;
-        int ans = 0;
-        
-        sort(words.begin(), words.end(), larger);
-        
-        
-        for(auto word:words)
-            mp[word]++;
-        
-        for(auto word:words)
-        {
-            int wordsize = word.size();
-            if(mp[word]>0)
-                ans += wordsize+1;
-            
-            for(int j=wordsize-1; j>=0; j--)
-            {
-                string s = word.substr(j, wordsize-j);
-                mp[s] = 0;
-            }
+    int minimumLengthEncoding(vector<string>& words) {
+        for(auto &word: words){
+            reverse(word.begin(), word.end());
         }
-        
-        return ans;
+        sort(words.begin(), words.end());
+        int i;
+        int n = words.size();
+        string str = words[0];
+        int count = 0, size;
+        for(i=1;i<n;i++){
+            size = str.size();
+            if(words[i].compare(0, size, str)){
+                count += 1 + size;
+            }
+            str = words[i];
+        }
+        count += str.size()+1;
+        return count;
     }
 };
