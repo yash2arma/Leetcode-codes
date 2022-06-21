13724d96cfaf40997d1bccca6ae5cc111daada61
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-06-21 16:27:21 +0530

    Time: 154 ms (49.65%), Space: 54 MB (27.49%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) 
     {
-        priority_queue<int> maxBricks;
+        priority_queue<int> maxBricks; //stores bricks used in each step in decreasing order
         
-        int i=0, diff=0;
+        int i=0, height_diff=0;
+        
+        //go until all bricks and ladders are used or reach at last building
         for(i=0; i<heights.size()-1; i++)
         {
-            diff = heights[i+1]-heights[i];
+            height_diff = heights[i+1]-heights[i];
             
-            if(diff<=0)
+            //if current building's height >= next building's height, we don't need a ladder  or bricks
+            if(height_diff<=0) 
                 continue;
             
-            bricks -= diff;
-            maxBricks.push(diff);
+            bricks -= height_diff; //subtract bricks used from total bricksj
+            maxBricks.push(height_diff); //add bricks used in priority_queue
             
-            if(bricks<0)
+            if(bricks<0) //if we don't have enough bricks we use ladder
             {
-                bricks += maxBricks.top();
+                bricks += maxBricks.top(); //add max bricks used in previous buildings to total bricks
                 maxBricks.pop();
-                ladders--;
+                ladders--; //and used ladder their
             }
             
-            if(ladders<0)
+            if(ladders<0) //if all ladders are used we break so currect building is ans
                 break;
         }
         return i;
