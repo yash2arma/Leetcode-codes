b665add99aa49d340dde9dd2bcbb4cf6a586e8a3
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-13 12:50:26 +0530

    Time: 11 ms (91.62%), Space: 5.9 MB (31.51%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(int x) 
-    {
+    {       
+        // Negative numbers cannot be palindrome
+        // Also if the last digit of the number is 0
+        // the first digit of the number also needs to be 0
+        // Only 0 satisfy this condition
         
-        if(x<0 || (x!=0) && x%10==0 ) return false;
+        if (x<0 || (x!=0 && x%10==0))
+            return false;
         
-        int rn = 0;
-        
-        while(x>rn)
-        {
-            rn = rn*10 + x%10;
-            x = x/10;
+        //initialize the reverse number...
+        int ReverseNumber = 0;
+
+        //process to reverse the number...
+        while (x > ReverseNumber){
+    	    ReverseNumber = ReverseNumber * 10 + x % 10;
+    	    x = x/10;
+        // if the reverse of half of the number is equal to the other half then it's a palindrome
+        // When x > ReverseNumber, we've reached the middle of the number
         }
+        // when length is an odd number, we need to get rid of the middle digit
+        // since the middle digit doesn't matter in palindrome
         
-        return (x==rn || rn/10 == x);
+        return (x == ReverseNumber || x == ReverseNumber/10);
+        
     }
 };
 
