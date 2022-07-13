12019ead33515f74df3f4c3cc4267d7ac490d61a
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-13 12:26:35 +0530

    Time: 11 ms (23.09%), Space: 9.6 MB (72.01%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -7,6 +7,56 @@ public:
         
         while(start<=end)
         {
+            mid = start + (end-start)/2;
+            
+            if(nums[mid]==target) return mid;
+            
+            else if(nums[mid] < target) start = mid+1;
+            
+            else end = mid-1;
+                
+        }
+        return start;
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int start=0, end=nums.size()-1;
+        int mid;
+        
+        while(start<=end)
+        {
             mid=start+(end-start)/2;
             
             if(nums[mid]<target)
@@ -20,3 +70,4 @@ public:
         
     }
 };
+*/
