79912c65cf411471cfa4cc8145e8503140a0335d
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-14 15:09:47 +0530

    Time: 30 ms (94.94%), Space: 25.5 MB (33.82%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     void rotate(vector<int>& nums, int k) 
     {
@@ -21,3 +21,19 @@ public:
     
     }
 };
+*/
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        int n=nums.size();
+        vector<int> temp(n);  
+        for(int i=0; i<n; i++)
+            temp[(i+k)%n] = nums[i];
+        nums = temp;
+    
+    }
+};
+
