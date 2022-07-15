18c1757609065d56d4ea411f352313f9121d7e84
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-15 11:23:18 +0530

    Time: 24 ms (34.74%), Space: 15.5 MB (92.75%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
+    //Use 2 pointers approach
+    //Given array is already sorted
     vector<int> twoSum(vector<int>& nums, int tar) 
     {
+        //define start and end pointer
         int s=0, e=nums.size()-1;
+        
+        //iterate until both pointers reach at the same index
         while(s<e)
         {
             if(nums[s]+nums[e]==tar) break;
@@ -11,6 +16,7 @@ public:
             
             else s++;
         }
+        //return the index of s and e added by one
         return {s+1, e+1};           
     }
 };
