d78c2b49f81979b5e44ca030dba5d20997a099e5
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-15 15:25:33 +0530

    Time: 7 ms (31.95%), Space: 6.8 MB (30.34%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -6,37 +6,23 @@ public:
         int x, y;
         int carry=0, sum=0;
         string res="";
-        while(i>=0 && j>=0)
+        while(i>=0 || j>=0 || carry)
         {
-            x = a[i]-'0';
-            y = b[j]-'0';
-            sum = x+y+carry;
+            sum = carry;
+            if(i>=0)
+            {
+                sum += a[i]-'0';
+                i--;
+            }
+            if(j>=0)
+            {
+                sum += b[j]-'0';
+                j--;
+            }
+
             res = to_string(sum%2) + res;
             carry = sum/2;
-            i--;
-            j--;
         }
-        
-        while(i>=0)
-        {
-            x = a[i]-'0';
-            sum = x+carry;
-            res = to_string(sum%2) + res;
-            carry = sum/2;
-            i--;
-        }
-        
-        while(j>=0)
-        {
-            y = b[j]-'0';
-            sum = y+carry;
-            res = to_string(sum%2) + res;
-            carry = sum/2;
-            j--;
-        }
-        
-        if(carry==1)
-            res = "1" + res;
             
         return res;
     }
