fd42756b3d9c9b2a32e35c794ae771e4a3284eca
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-15 10:48:20 +0530

    Time: 24 ms (74.49%), Space: 23.2 MB (67.07%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    void help(int r, int c, int m, int n, vector<vector<int>>& grid, int &maxi_area, int &sum)
+    {
+        if(r>=m || r<0 || c>=n || c<0 || grid[r][c]==-1 || grid[r][c]==0)
+            return;
+        
+        sum += grid[r][c];
+        grid[r][c] = -1;
+        
+        help(r, c-1, m, n, grid, maxi_area, sum);
+        help(r, c+1, m, n, grid, maxi_area, sum);
+        help(r-1, c, m, n, grid, maxi_area, sum);
+        help(r+1, c, m, n, grid, maxi_area, sum);
+        
+        maxi_area = max(maxi_area, sum);
+    }
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) 
+    {
+        int maxi_area = 0, sum; 
+        int m=grid.size(), n=grid[0].size();
+        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                sum = 0;
+                help(i, j, m, n, grid, maxi_area, sum);
+            }
+        }
+        return maxi_area;
+        
+    }
+};
