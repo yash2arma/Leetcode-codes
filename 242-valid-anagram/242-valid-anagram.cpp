91111339cb84ccf76d419198613a15d1e92191a6
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-19 15:06:25 +0530

    Time: 33 ms (5.61%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,12 +3,16 @@ public:
     bool isAnagram(string s, string t) 
     {
         if(s.size()!=t.size()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
         
-        for(int i=0; i<t.size(); i++)
+        map<char, int> mp;
+        
+        for(auto it:t)
+            mp[it]++;
+        
+        for(auto it:s)
         {
-            if(s[i]!=t[i]) return false;
+            mp[it]--;
+            if(mp.find(it)==mp.end() || mp[it]<0) return false;
         }
         return true;
         
