4a76403b6a829b24a3b08b7c8822da975abe490e
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-21 15:59:39 +0530

    Time: 168 ms (22.53%), Space: 30 MB (70.94%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     vector<int> DIR = {0, 1, 0, -1, 0};
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
+    {
+        vector<int> dir = {0, 1, 0, -1, 0};
         int m = mat.size(), n = mat[0].size();
         queue<pair<int, int>> q;
-        for (int r = 0; r < m; ++r)
-            for (int c = 0; c < n; ++c)
-                if (mat[r][c] == 0) q.emplace(r, c);
-                else mat[r][c] = -1; // Marked as not processed yet!
-
-        while (!q.empty()) {
-            auto [r, c] = q.front(); q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int nr = r + DIR[i], nc = c + DIR[i+1];
-                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
-                mat[nr][nc] = mat[r][c] + 1;
-                q.emplace(nr, nc);
+        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(mat[i][j]==0) q.push({i, j});
+                else mat[i][j] = -1; // Marked as not processed yet!
             }
         }
+        
+        while(!q.empty())
+        {
+            auto [r,c] = q.front(); q.pop();
+            
+            for(int i=0; i<4; i++)
+            {
+                int cur_r = r+dir[i];
+                int cur_c = c+dir[i+1];
+                
+                if(cur_r<0 || cur_r==m || cur_c<0 || cur_c==n || mat[cur_r][cur_c]!=-1)
+                    continue;
+                
+                mat[cur_r][cur_c] = 1 + mat[r][c];
+                q.push({cur_r, cur_c});
+            }
+        }
+
+        
         return mat;
     }
+    
 };
