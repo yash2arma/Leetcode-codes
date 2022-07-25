f9bec92f48ed54181a9b3b776ca4fcb7d72af6eb
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-25 13:40:28 +0530

    Time: 15 ms (96.18%), Space: 5.8 MB (71.67%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     bool isPowerOfThree(int n) 
     {
         if(n==0) return false;
@@ -8,5 +9,12 @@ public:
             n /= 3;
         }
         return n==1;
+    }
+    */
+    
+    bool isPowerOfThree(int n) 
+    {
+        return n > 0 && 1162261467 % n == 0;
+	    // pow(3, floor(log(INT_MAX)/log(3))) = 1162261467 
     }
 };
