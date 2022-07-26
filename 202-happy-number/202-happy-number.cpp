d159d51e3ae1e516030fbbb6b953b4fcc134f8ab
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-07-26 18:14:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (63.51%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isHappy(int n) 
+    /*bool isHappy(int n) 
     {
         int count=20;
         int sum;
@@ -16,7 +16,34 @@ public:
             n = sum;
             if(n==1) return true; 
         }
-        return false;
+        return false;    
+    }
+    */
+    
+    int digitSquareSum(int n) 
+    {
+        int sum = 0, tmp;
+        while (n) 
         
+        {
+            tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) 
+    {
+        int slow, fast;
+        slow = fast = n;
+        do 
+        {
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(digitSquareSum(fast));
+        } 
+        while(slow != fast);
+        if (slow == 1) return 1;
+        else return 0;
     }
 };
