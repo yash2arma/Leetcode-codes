5a44cb990b00313209e540e8340c2a3ec712d8ea
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-08-05 11:55:49 +0530

    Time: 6 ms (27.16%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,13 +1,14 @@
 class Solution 
 {
 public:
-    
-    int help(int idx, vector<int>& nums, int tar, vector<vector<int>>& dp)
+    //1. Memoization: 
+    /*
+    int help(int idx, vector<int>& nums, int tar, vector<int>& dp)
     {
         if(tar<0 || idx>=nums.size()) return 0;
         if(tar==0) return 1;
         
-        if(dp[idx][tar]!=-1) return dp[idx][tar];
+        if(dp[tar]!=-1) return dp[tar];
         
         int count=0;
         for(int i=0; i<nums.size(); i++)
@@ -15,18 +16,38 @@ public:
             if(nums[i] <= tar)
                 count += help(i, nums, tar-nums[i], dp);
         }
-        return dp[idx][tar] = count;
+        return dp[tar] = count;
         
     }
     int combinationSum4(vector<int>& nums, int target) 
     {
         int count = 0;
-        vector<vector<int>> dp(nums.size(), vector<int> (target, -1));
+        vector<int> dp(target+1, -1);
         for(int i=0; i<nums.size(); i++)
         {
             count += help(i, nums, target-nums[i], dp);
         }
+        
         return count;        
+    }
+    */
+    
+    //2. Tabulation
+
+    int combinationSum4(vector<int>& nums, int target) 
+    {
+        vector<unsigned long int> dp(target + 1);
+        dp[0] = 1;
+        sort (nums.begin(), nums.end());
+        for (int i = 1; i <= target; i++) 
+        {
+            for (auto it : nums) 
+            {
+                if (i < it) break;
+                dp[i] += dp[i - it];
+            }
+        }
+        return dp.back();
     }
     
 };
