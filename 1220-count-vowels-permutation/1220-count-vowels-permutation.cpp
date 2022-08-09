d3f707e9ca5ddec98984432102ab87ba71ef9be3
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-08-09 11:03:58 +0530

    Time: 2 ms (95.95%), Space: 5.9 MB (90.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,23 +1,33 @@
-static vector<vector<int>> relation={{1,2,4},{0,2},{1,3},{2},{2,3}};  //Relationships(can be followed after) of each vowel with others
+/*
+Each vowel allows some number of subsequent characters. These transitions are like a tree. This problem is asking, "what's the width of the tree with height n?"
+
+a -> e
+e -> a, i
+i -> a, e, o, u
+o -> i, u
+u -> a
+
+My solution keeps track of the number of each vowel at a level in this tree. To calculate say 'A', we calculate how many nodes in the previous level produce 'A'. This is the number of 'E', 'I', and 'U' nodes.
+*/
+
 class Solution {
 public:
     int countVowelPermutation(int n) 
     {
-        int MOD=1e9+7;
-        vector<long long> vowels(5,1),vowels_copy;      //When N=1 all vowels are used once to for 1 letter strings
-        long long result=0;
-        while(--n)
+        long a=1, e=1, i=1, o=1, u=1, mod=1e9+7;
+        long a2, e2, i2, o2, u2;
+        
+        for(int j=2; j<=n; j++)
         {
-            vowels_copy=vowels;
-            for(int i=0;i<5;i++)                         //Characters 'a' 'e' 'i' 'o' 'u'.
-            {
-                vowels[i]=0;
-                for(int &r:relation[i])
-                    vowels[i]+=vowels_copy[r],vowels[i]%=MOD;  //Add the strings that end with characters that can have 'i' after them.
-            }
+            a2 = (e+i+u)%mod;
+            e2 = (a+i)%mod;
+            i2 = (e+o)%mod;
+            o2 = (i)%mod;
+            u2 = (i+o)%mod;
+            
+            a=a2, e=e2, i=i2, o=o2, u=u2;
         }
-        for(long long &i:vowels)
-            result+=i,result%=MOD;
-        return result;
+        return (a+e+i+o+u)%mod;
     }
 };
+
