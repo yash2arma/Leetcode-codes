3d21bd8d8cc1521e9eac79993aecb97a2eac2f13
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-08-13 10:28:47 +0530

    Time: 822 ms (36.23%), Space: 292.5 MB (20.48%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,42 @@
+class Solution 
+{
+public:
+    vector<int> findSubstring(string s, vector<string>& words) 
+    {
+        vector<int> res;
+        int n=words.size();
+        int m=words[0].size(); // each word has the same length;
+        int t = m*n;
+        
+        if(s.size() < t) return res;
+        
+        unordered_map<string, int> freq;
+            
+        for(auto it:words)
+            freq[it]++;
+        
+        for(int i=0; i<=s.size()-t; i++)
+        {
+            unordered_map<string, int> mp = freq;
+            
+            int j;
+            for(j=0; j<s.size(); j++)
+            {
+                string temp = s.substr(i+j*m, m);
+                
+                if(mp[temp]==0)
+                    break;
+                else
+                {
+                    mp[temp]--;
+                    //else break;
+                }
+            }
+            if(j==n)
+                res.push_back(i);
+            
+        }
+        return res;
+        
+    }
+};
