47a610fa6f2aac73bfe5c1c6c4ef894e7dc0f39f
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-08-15 12:35:06 +0530

    Time: 7 ms (71.90%), Space: 9 MB (25.88%) - LeetHub

--- 942-di-string-match/942-di-string-match.cpp
+++ 942-di-string-match/942-di-string-match.cpp
@@ -5,20 +5,10 @@ public:
     {
         int f=0, l=s.size();
         vector<int> ans;
-        for(int i=0; i<s.size(); i++)
-        {
-            if(s[i]=='I')
-            {
-                ans.push_back(f);
-                f++;
-            }
-            else
-            {
-                ans.push_back(l);
-                l--;
-            }
-        }
-        ans.push_back(l);
+        
+        for(int i=0; i<=s.size(); i++)
+            ans.push_back((i==s.size() || s[i]=='I') ? f++ : l--);
+
         return ans;
     }
 };
