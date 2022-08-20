8493c05f70e73e0911b969571545ba1db89d1a70
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-08-20 19:01:03 +0530

    Time: 18 ms (99.69%), Space: 16.2 MB (80.69%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,6 +1,8 @@
+/*
 class Solution 
 {
 public:
+    
     
     int minRefuelStops(int target, int startFuel, vector<vector<int>> s) 
     {
@@ -13,5 +15,49 @@ public:
             if (dp[t] >= target) return t;
         
         return -1;
+    }
+};
+*/
+
+//Approach-3 : O(nlog(n)) using max-heap
+//Similar Problem : Leetcode 1642. Furthest Building You Can Reach
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        int n = stations.size();
+        
+        priority_queue<int> pq; //max heap
+        /*
+			if we can't reach target, I will first use the largestfuel Station
+			(which can take me farthest) and move ahead and keep doing this.
+        */
+        int curr = startFuel; //my current position
+        int i = 0; //station index
+        int result = 0;
+        while(curr < target) { //until I reach my target
+            while(i < n && curr >= stations[i][0]) {
+                /*
+					  Sice, I am already ahead of or at this station
+					  (i.e. curr >= stations[i][0])  we don't use this
+					  station and try to move ahead but I store this
+					  station in maxheap to use in future
+				*/
+                pq.push(stations[i][1]);
+				//Keep the station at top which can take me farthest (maximum gas)
+                i++;
+            }
+            
+            if(pq.empty()) //we can't move further now
+                return -1;
+            
+            int dist = pq.top();
+            pq.pop();
+            
+            curr += dist;
+            result++;
+        }
+        
+        return result;
+        
     }
 };
