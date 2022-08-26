5cd9659a3b17559d7dc6648b0d71a39159c8f662
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-08-26 19:33:25 +0530

    Time: 25 ms (43.05%), Space: 10.4 MB (25.90%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,8 +1,34 @@
-//Tabulation
+
 class Solution 
 {
 public:
+    
+    //Memoizaton
+    int f(int i, int j, vector<vector<int>> &mat, vector<vector<int>> &dp)
+    {
+        if(j<0 || j>=mat[0].size()) return 1e8;
+        if(i==0) return mat[0][j];
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int u=mat[i][j] + f(i-1, j, mat, dp);
+        int ld=mat[i][j] + f(i-1, j-1, mat, dp);
+        int rd=mat[i][j] + f(i-1, j+1, mat, dp);
+
+        return dp[i][j] = min(u, min(ld, rd));
+    }
+
     int minFallingPathSum(vector<vector<int>> &mat)
+    {
+        int m=mat.size(), n=mat[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        int mini=1e8;
+        for(int j=0; j<n; j++)
+            mini = min(mini, f(m-1, j, mat, dp));
+        return mini;
+    }
+    
+    //Tabulation
+    /*int minFallingPathSum(vector<vector<int>> &mat)
     {
         int m=mat.size(), n=mat[0].size();
         vector<vector<int>> dp(m, vector<int> (n, 0));
@@ -31,12 +57,10 @@ public:
             mini = min(mini, dp[m-1][j]);
         return mini;
     }
-};
-
-
-//Space Optimization
-/*class Solution {
-public:
+    */
+    
+    //Space Optimization
+    /*
     int minFallingPathSum(vector<vector<int>>& mat) 
     {
         int r=mat.size(), c=mat[0].size();
@@ -70,5 +94,7 @@ public:
         return mini;
         
     }
+    */
 };
-*/
+
+
