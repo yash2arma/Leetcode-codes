bce04e8ed7255a346de3bdca487307685d4d4176
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-08-26 19:29:05 +0530

    Time: 15 ms (89.71%), Space: 10.3 MB (54.06%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,4 +1,41 @@
-class Solution {
+//Tabulation
+class Solution 
+{
+public:
+    int minFallingPathSum(vector<vector<int>> &mat)
+    {
+        int m=mat.size(), n=mat[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+
+        for(int j=0; j<n; j++) dp[0][j] = mat[0][j];
+
+        for(int i=1; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                int u = mat[i][j] + dp[i-1][j];
+                int ld = mat[i][j], rd = mat[i][j];
+
+                if(j-1>=0) ld += dp[i-1][j-1];
+                else ld += 1e8;
+
+                if(j+1<n) rd += dp[i-1][j+1];
+                else rd += 1e8;
+
+                dp[i][j] = min(u, min(ld, rd));
+            }
+        }
+
+        int mini=1e8;
+        for(int j=0; j<n; j++)
+            mini = min(mini, dp[m-1][j]);
+        return mini;
+    }
+};
+
+
+//Space Optimization
+/*class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& mat) 
     {
@@ -34,3 +71,4 @@ public:
         
     }
 };
+*/
