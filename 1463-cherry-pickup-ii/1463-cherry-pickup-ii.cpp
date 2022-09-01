14c038785831a193fb871251d13f59cab6184265
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-09-01 13:05:35 +0530

    Time: 251 ms (32.35%), Space: 16.9 MB (50.28%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -2,6 +2,8 @@ class Solution
 {
 public:
     
+    //1. Memoization
+    /*
     int f(int i, int j1, int j2, int r, int c, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp)
     {
         if(j1<0 || j1>=c || j2<0 || j2>=c) return -1e8;
@@ -31,5 +33,56 @@ public:
         int r=grid.size(), c=grid[0].size();
         vector<vector<vector<int>>> dp(r, vector<vector<int>> (c, vector<int> (c, -1)));
         return f(0, 0, c-1, r, c, grid, dp);
+    }
+    */
+    
+    
+    //2. Tabulation
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int r=grid.size(), c=grid[0].size();
+        vector<vector<vector<int>>> dp(r, vector<vector<int>> (c, vector<int> (c, 0)));
+
+        for(int j1=0; j1<c; j1++)
+        {
+            for(int j2=0; j2<c; j2++)
+            {
+                if(j1==j2) dp[r-1][j1][j2]=grid[r-1][j1];
+                else dp[r-1][j1][j2]=grid[r-1][j1]+grid[r-1][j2];
+            }
+        }
+
+        //all possible ways
+        for(int i=r-2; i>=0; i--)
+        {
+            for(int j1=0; j1<c; j1++)
+            {
+                for(int j2=0; j2<c; j2++)
+                {
+                    int maxi=INT_MIN;
+                    for(int dj1=-1; dj1<=1; dj1++)
+                    {
+                        for(int dj2=-1; dj2<=1; dj2++)
+                        {
+                            int ans=0;
+                            int x1=j1+dj1, x2=j2+dj2;
+
+                            if(j1==j2) ans=grid[i][j1];
+                            else ans=grid[i][j1] + grid[i][j2];
+
+                            if(x1>=0 && x1<c && x2>=0 && x2<c)
+                                ans += dp[i+1][x1][x2];
+                            else
+                                ans += -1e8;
+
+                             maxi = max(maxi, ans);  
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][c-1];
+
     }
 };
