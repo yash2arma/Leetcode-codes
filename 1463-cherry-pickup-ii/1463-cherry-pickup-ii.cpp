6b4d24ef7497768b6fcdeb5687cc14843b98671c
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-09-01 13:23:36 +0530

    Time: 253 ms (31.86%), Space: 9.3 MB (97.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -38,6 +38,7 @@ public:
     
     
     //2. Tabulation
+    /*
     int cherryPickup(vector<vector<int>>& grid) 
     {
         int r=grid.size(), c=grid[0].size();
@@ -83,6 +84,57 @@ public:
             }
         }
         return dp[0][0][c-1];
+    }
+    */
+    
+    //3. Space Optimization
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int r=grid.size(), c=grid[0].size();
+        vector<vector<int>> front(c, vector<int> (c, 0));
+        vector<vector<int>> cur(c, vector<int> (c, 0));
+
+        for(int j1=0; j1<c; j1++)
+        {
+            for(int j2=0; j2<c; j2++)
+            {
+                if(j1==j2) front[j1][j2]=grid[r-1][j1];
+                else front[j1][j2]=grid[r-1][j1]+grid[r-1][j2];
+            }
+        }
+
+        //all possible ways
+        for(int i=r-2; i>=0; i--)
+        {
+            for(int j1=0; j1<c; j1++)
+            {
+                for(int j2=0; j2<c; j2++)
+                {
+                    int maxi=INT_MIN;
+                    for(int dj1=-1; dj1<=1; dj1++)
+                    {
+                        for(int dj2=-1; dj2<=1; dj2++)
+                        {
+                            int ans=0;
+                            int x1=j1+dj1, x2=j2+dj2;
+
+                            if(j1==j2) ans=grid[i][j1];
+                            else ans=grid[i][j1] + grid[i][j2];
+
+                            if(x1>=0 && x1<c && x2>=0 && x2<c)
+                                ans += front[x1][x2];
+                            else
+                                ans += -1e8;
+
+                             maxi = max(maxi, ans);  
+                        }
+                    }
+                    cur[j1][j2] = maxi;
+                }
+            }
+            front = cur;
+        }
+        return front[0][c-1];
 
     }
 };
