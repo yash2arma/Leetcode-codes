3663d18bb5821513bd57c53c3083e74da2c0d933
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-09-20 12:46:22 +0530

    Time: 537 ms (43.97%), Space: 11.9 MB (92.72%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution 
 {
 public:
-    int findLength(vector<int>& nums1, vector<int>& nums2) 
+    /*int findLength(vector<int>& nums1, vector<int>& nums2) 
     {
         int maxi=0;
         int n=nums1.size(), m=nums2.size();
@@ -17,6 +17,37 @@ public:
                     maxi = max(maxi, dp[i][j]);
                 }
             }
+        }
+        return maxi;
+    }
+    */
+    
+    int findLength(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int maxi=0;
+        int n=nums1.size(), m=nums2.size();
+        if(n<m)
+        {
+            swap(nums1, nums2);
+            swap(n, m);
+        }
+        cout<<n<<" "<<m;
+        vector<int> pre(m+1), cur(m+1);
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(nums1[i-1]==nums2[j-1])
+                    cur[j] = pre[j-1] + 1;
+                
+                else
+                    cur[j] = 0;
+                
+                maxi = max(maxi, cur[j]);
+                    
+            }
+            pre = cur;
         }
         return maxi;
     }
