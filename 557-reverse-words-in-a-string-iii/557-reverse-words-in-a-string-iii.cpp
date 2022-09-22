03cc60835b49a3878bff98d36e3a32f4dae8a543
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-09-22 18:53:55 +0530

    Time: 106 ms (9.25%), Space: 48.2 MB (9.03%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,5 +1,52 @@
 class Solution {
 public:
+    
+    string reverseWords(string s) 
+    {
+        string res="", temp="";
+        int low=0, n=s.size();
+        
+        while(low<n)
+        {
+            if(s[low]!=' ')
+            {
+                temp = s[low] + temp;
+            }
+            else if(s[low]==' ')
+            {
+                res += temp + " ";
+                temp="";
+            }
+            low++;
+        }
+        res += temp;
+        return res;
+        
+    }
+    /*string reverseWords(string s) 
+    {
+        string res = "";
+        string temp = "";
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]==' ')
+            {
+                res = res + temp + " ";
+                temp = "";
+            }
+            else
+                temp = s[i] + temp;
+            
+        }
+        if(temp!="")
+            res = res + temp;
+        return res;
+        
+    }
+    */
+    
+    /*
     string reverseWords(string s) 
     {
         int low=0;
@@ -17,4 +64,5 @@ public:
         return s;
         
     }
+    */
 };
