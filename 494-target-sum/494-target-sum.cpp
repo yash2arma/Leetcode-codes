3e5dd66e7ebc8b6a91a5298846cbdf8f5b94a432
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-09-26 14:32:09 +0530

    Time: 36 ms (54.26%), Space: 12.1 MB (45.56%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,6 +1,8 @@
 class Solution 
 {
 public:
+    //1. Memoization
+    /*
     int findways(int i, vector<int>&arr, int tar, vector<vector<int>> &dp)
     {
         if(i==0)
@@ -34,6 +36,43 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) 
     {
         return countPartitions(nums.size(), target, nums);
+    }
+    */
+    
+    
+    //2. Tabulation
+    int findTargetSumWays(vector<int>& arr, int tar) 
+    {
+        int n=arr.size();
+        int sum=0;
+        for(auto it:arr) sum += it;
+        
+        if((sum-tar)<0 || (sum-tar)%2) return 0;
+        
+        int k=(sum-tar)/2;
+        
+        vector<vector<int>> dp(n, vector<int> (k+1, 0));
+    
+        if(arr[0]==0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+        
+        if(arr[0]!=0 && arr[0]<=k) dp[0][arr[0]]=1;
+        
+        for(int i=1; i<n; i++)
+        {
+            for(int t=0; t<=k; t++)
+            {
+                int not_pick=0, pick=0;
+                not_pick = dp[i-1][t];
+                
+                if(arr[i] <= t)
+                    pick = dp[i-1][t-arr[i]];
+
+                dp[i][t] = pick+not_pick;
+            }
+        }
+        return dp[n-1][k];
+        
     }
 	  
 };
