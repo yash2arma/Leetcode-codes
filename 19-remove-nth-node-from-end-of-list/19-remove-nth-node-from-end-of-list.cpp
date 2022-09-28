d8e62fc4947d7bbcc0221e0e196f7fdb5309c767
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-09-28 10:20:48 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (32.26%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -8,7 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution 
+{
 public:
     ListNode* removeNthFromEnd(ListNode* head, int K) 
     {
