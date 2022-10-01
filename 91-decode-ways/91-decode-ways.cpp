7dd7ba2c7a442563f38bd3f0622c13f53fb3236e
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-01 10:33:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (28.47%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,28 +1,45 @@
-class Solution 
+class Solution
 {
-public:
-    int decode(int idx, string s, vector<int> &dp)
-    {
-        if(idx == s.size()) return 1;
-            
-        if(s[idx]=='0') return 0;
-        
-        if(dp[idx]!=-1) return dp[idx];
-        
-        int res = decode(idx+1, s, dp);
-        
-        if(idx < s.size()-1 && (s[idx]=='1' || (s[idx]=='2' && s[idx+1]<='6')))
-            res += decode(idx+2, s, dp);
-        
-        return dp[idx] = res;
-        
-    }
-    
-    int numDecodings(string s) 
-    {
-        int n=s.size();
-        vector<int> dp(n+1, -1);
-        dp[n]=1;
-        return decode(0, s, dp);    
-    }
+    public:
+        /*int decode(int idx, string s, vector<int> &dp)
+        {
+            if(idx == s.size()) return 1;
+
+            if(s[idx]=='0') return 0;
+
+            if(dp[idx]!=-1) return dp[idx];
+
+            int res = decode(idx+1, s, dp);
+
+            if(idx < s.size()-1 && (s[idx]=='1' || (s[idx]=='2' && s[idx+1]<='6')))
+                res += decode(idx+2, s, dp);
+
+            return dp[idx] = res;
+        }
+
+        int numDecodings(string s) 
+        {
+            int n=s.size();
+            vector<int> dp(n+1, -1);
+            dp[n]=1;
+            return decode(0, s, dp);    
+        }
+        */
+
+        int numDecodings(string s)
+        {
+            int n = s.size();
+            vector<int> dp(n + 1);
+            dp[n] = 1;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (s[i] == '0') dp[i] = 0;
+                else
+                {
+                    dp[i] = dp[i + 1];
+                    if (i < n - 1 && (s[i] == '1' || s[i] == '2' && s[i + 1]<'7')) dp[i] += dp[i + 2];
+                }
+            }
+            return dp[0];
+        }
 };
