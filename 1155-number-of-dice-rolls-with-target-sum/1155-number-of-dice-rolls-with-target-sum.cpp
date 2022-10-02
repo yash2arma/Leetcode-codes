ce44e342ff69e7e4281e47b15d0346d52bc74f85
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-02 19:23:43 +0530

    Time: 85 ms (25.75%), Space: 8.6 MB (28.55%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,23 +2,44 @@ class Solution
 {
     public:
         int mod = 1e9+7;
-
-    /*int help(int i, int k, int tar, vector<vector<int>> &dp)
+    
+//     int numRollsToTarget(int n, int k, int target)
+//     {
+//         vector<vector<int>> dp(n+1, vector<int> (target+1, 0));
+//         dp[0][0] = 1;
+        
+//         for(int i=1; i<=n; i++)
+//         {
+//             for(int t=0; t<=target; t++)
+//             {
+//                 for(int j=1; j<=k; j++)
+//                 {
+//                     if(j<=t)
+//                         dp[i][t] = (dp[i][t]%mod + dp[i-1][t-j]%mod)%mod;
+//                 }
+//             }
+//         }
+//         return dp[n][target]%mod;
+//     }
+    
+    
+    int help(int i, int &k, int tar, vector<vector<int>> &dp)
     {
-        if (i==0 || tar<=0)
+        if (i==0)
         {
             return tar==0 ? 1:0;
         }
 
-        if (dp[i][tar] != -1) dp[i][tar];
+        if (dp[i][tar] != -1) return dp[i][tar];
         int take = 0;
+        
         for (int n=1; n<=k; n++)
         {
             if (n <= tar)
-                take = (take%mod + help(i - 1, k, tar - n, dp)%mod) % mod;
+                take = (take%mod + help(i-1, k, tar-n, dp)%mod) % mod;
         }
 
-        return dp[i][tar] = take;
+        return dp[i][tar] = take%mod;
     }
 
     int numRollsToTarget(int n, int k, int target)
@@ -26,24 +47,5 @@ class Solution
         vector<vector<int>> dp(n+1, vector<int> (target+1, -1));
         return help(n, k, target, dp) % mod;
     }
-    */
     
-    int numRollsToTarget(int n, int k, int target)
-    {
-        vector<vector<int>> dp(n+1, vector<int> (target+1, 0));
-        dp[0][0] = 1;
-        
-        for(int i=1; i<=n; i++)
-        {
-            for(int t=0; t<=target; t++)
-            {
-                for(int j=1; j<=k; j++)
-                {
-                    if(j<=t)
-                        dp[i][t] = (dp[i][t]%mod + dp[i-1][t-j]%mod)%mod;
-                }
-            }
-        }
-        return dp[n][target]%mod;
-    }
 };
