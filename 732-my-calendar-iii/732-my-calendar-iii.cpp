c8e23b798029b25fadb3f1401e43d702b78ba2a6
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-08 01:38:18 +0530

    Time: 287 ms (24.16%), Space: 26.3 MB (76.58%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -0,0 +1,24 @@
+class MyCalendarThree {
+public:
+    map<int, int> timeline;
+    MyCalendarThree() 
+    {
+    }
+    
+    int book(int s, int e) 
+    {
+        timeline[s]++; // 1 new event will be starting at [s]
+        timeline[e]--; // 1 new event will be ending at [e];
+        int ongoing = 0, k = 0;
+        for (pair<int, int> t : timeline)
+            k = max(k, ongoing += t.second);
+        return k;
+    }
+};
+
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
