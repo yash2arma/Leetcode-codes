e8f0c5325aaffa52cfe16df795d4544d87122a13
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-14 14:54:16 +0530

    Time: 47 ms (75.81%), Space: 30 MB (27.15%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     
+    //1. Memoization
+    /*
     int help(int i, int j, string &s, string &t, vector<vector<int>> &dp)
     {
         if(j<0) return 1;
@@ -19,5 +21,28 @@ public:
         int m=s.size(), n=t.size();
         vector<vector<int>> dp(m, vector<int> (n, -1));
         return help(m-1, n-1, s, t, dp);
+    }
+    */
+    
+    //2. Tabulation
+    int numDistinct(string s, string t) 
+    {
+        int m=s.size(), n=t.size();
+        vector<vector<double>> dp(m+1, vector<double> (n+1, 0));
+        
+        for(int i=0; i<=m; i++)
+            dp[i][0] = 1;
+        
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return (int)dp[m][n];
     }
 };
