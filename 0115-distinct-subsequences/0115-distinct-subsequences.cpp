5a94ff81d1c19eb9e69fe78e95577f2cdf53b72e
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-14 15:10:42 +0530

    Time: 49 ms (73.22%), Space: 6.5 MB (88.92%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -49,7 +49,8 @@ public:
     */
     
     //3. Space Optimization 2d
-    int numDistinct(string s, string t) 
+    
+    /*int numDistinct(string s, string t) 
     {
         int m=s.size(), n=t.size();
         vector<double> pre(n+1, 0), cur(n+1, 0);
@@ -68,5 +69,27 @@ public:
             pre = cur;
         }
         return (int)pre[n];
+    }
+    */
+    
+    //4. Space Optimization 1d
+    int numDistinct(string s, string t) 
+    {
+        int m=s.size(), n=t.size();
+        vector<double> dp(n+1, 0);
+        
+        dp[0]=1;
+        
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=n; j>=1; j--)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[j] = dp[j-1] + dp[j];
+                else
+                    dp[j] = dp[j];
+            }
+        }
+        return (int)dp[n];
     }
 };
