2df595dedba3b26d24b3c6ae1b836d5745a337d1
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-15 21:40:54 +0530

    Time: 3 ms (63.64%), Space: 5.8 MB (72.73%) - LeetHub

--- 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
@@ -5,31 +5,34 @@ public:
     {   
         int ans=1;
         
-        if(time[0]=='?' && time[1]=='?') //for HH
+        if(time[0]=='?' && time[1]=='?') //when first both HH are ?? of (HH:MM)
                 ans *= 24;   
         
-        if(time[0]=='?' && time[1]!='?') 
+        else
         {
-            if(time[1] < '4') 
-                ans *= 3;
-            else
-                ans *= 2;
-        }
+            if(time[0]=='?')  //when first H is ? and second H has some value of (HH:MM)
+            {
+                if(time[1] < '4') 
+                    ans *= 3;
+                else
+                    ans *= 2;
+            }
 
-        if(time[1]=='?' && time[0]!='?')
-        {
-            if(time[0] < '2')
-                ans *= 10;
-            else
-                ans *= 4;
+            if(time[1]=='?') //when second H is ? and first H has some value of (HH:MM)
+            {
+                if(time[0] < '2')
+                    ans *= 10;
+                else
+                    ans *= 4;
+            }
         }
         
-        if(time[3]=='?')
+        if(time[3]=='?') //for first M of (HH:MM)
         {
             ans *= 6;
         }
 
-        if(time[4]=='?')
+        if(time[4]=='?') // for second M of (HH:MM)
         {
             ans *= 10;
         }       
