6c3607d50fe1f66f223d2b071c02407ca55fb7bf
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-15 15:10:12 +0530

    Time: 176 ms (20.80%), Space: 9.5 MB (77.64%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -34,6 +34,7 @@ public:
     */
     
     //1(b). Memoization - 1 based indexing
+    /*
     bool f(int i, int j, string &p, string &t, vector<vector<int>> &dp)
     {
         if(i==0 && j==0) return true;
@@ -56,47 +57,57 @@ public:
         return dp[i][j] = false;
     }
     
+    
     bool isMatch(string s, string p) 
     {
         int m=s.size(), n=p.size();
         vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
         return f(n, m, p, s, dp);
     }
+    */
     
     //2. Tabulation
-//     bool isMatch(string s, string p) 
-//     {
-//         int m=s.size(), n=p.size();
-//         vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
+    bool isMatch(string s, string p) 
+    {
+        int n=s.size(), m=p.size();
+        vector<vector<bool>> dp(m+1, vector<bool> (n+1, false));
         
-//         dp[0][0]=1;
+        dp[0][0]=true;
+        for(int j=1; j<=n; j++)
+            dp[0][j] = false;
+        
+        for(int i=1; i<=m; i++)
+        {
+            int flag = true;
+            for(int ii=1; ii<=i; ii++)
+            {
+                if(p[ii-1] != '*') 
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
         
         
-//         int k=0;
-//         while(true)
-//         {
-//             if(p[k]=='*') dp[k][0]=1;
-//             else break;
-//             k++;
-//         }
-        
-//         for(int i=1; i<=m; i++)
-//         {
-//             for(int j=1; j<=n; j++)
-//             {
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
                  
-//                 if(p[i-1]==s[j-1] || p[i-1]=='?')
-//                     dp[i][j] = dp[i-1][j-1];
+                if(p[i-1]==s[j-1] || p[i-1]=='?')
+                    dp[i][j] = dp[i-1][j-1];
 
-//                 else if(p[i-1]=='*')
-//                     dp[i][j] = dp[i-1][j] | dp[i][j-1];
-//                 else
-//                     dp[i][j] = 0;
+                else if(p[i-1]=='*')
+                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                else
+                    dp[i][j] = false;
         
-//             }
+            }
             
-//         }
-//         return dp[m][n];
-//     }
+        }
+        return dp[m][n];
+    }
     
 };
