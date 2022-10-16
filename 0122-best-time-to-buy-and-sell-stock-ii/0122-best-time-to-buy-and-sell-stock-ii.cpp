234bf3173d8aa848beeb8ad0c6d3406bec2c2d8e
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-16 14:07:49 +0530

    Time: 41 ms (5.72%), Space: 15.2 MB (21.19%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,6 +2,8 @@ class Solution
 {
 public:
     
+    //1. Memoization
+    /*
     int f(int i, vector<int>& prices, int buy, vector<vector<int>> &dp)
     {
         if(i==prices.size()) return 0;
@@ -25,5 +27,33 @@ public:
     {
         vector<vector<int>> dp(prices.size(), vector<int> (2, -1));
         return f(0, prices, 1, dp);
+    }
+    */
+    
+    //2. Tabulation
+    int maxProfit(vector<int>& prices) 
+    {
+        int n=prices.size();
+        vector<vector<int>> dp(n+1, vector<int> (2, 0));
+        dp[n][1] = dp[n][0] = 0;
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int buy=0; buy <= 1; buy++)
+            {
+                int profit=0;
+                if(buy) //buying stock
+                {
+                    profit = max(-prices[i] + dp[i+1][0], 0 + dp[i+1][1]);
+
+                }
+                else //selling stock
+                {
+                    profit = max(prices[i] + dp[i+1][1], 0 + dp[i+1][0]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
