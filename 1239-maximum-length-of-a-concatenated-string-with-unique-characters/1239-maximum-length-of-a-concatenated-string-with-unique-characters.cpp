864cd59438a0155d4c9b4fd3ae6ba8ccd219a1de
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-24 13:10:54 +0530

    Time: 119 ms (55.08%), Space: 57.6 MB (46.33%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,45 @@
+class Solution 
+{
+public:
+    
+    bool check(string &a, string &b)
+    {
+        vector<int> vis(26, 0);
+        for(auto it:a)
+            vis[it-'a']++;
+        
+        for(int i=0; i<a.size(); i++)
+        {
+            if(vis[a[i]-'a'] > 1) return false;
+            for(int j=0; j<b.size(); j++)
+            {
+                if(a[i]==b[j]) return false;
+            }
+        }
+        return true;
+    }
+    
+    int f(int i, vector<string>& arr, string str)
+    {
+        if(i==0)
+        {
+            if(check(arr[0], str))
+                return arr[0].size();
+            return 0;
+        }
+        
+        int take=0, not_take=0;
+        
+        not_take = 0 + f(i-1, arr, str);
+        
+        if(check(arr[i], str))
+            take = arr[i].size() + f(i-1, arr, str+arr[i]);
+        
+        return max(not_take, take);
+    }
+    
+    int maxLength(vector<string>& arr) 
+    {
+        return f(arr.size()-1, arr, "");
+    }
+};
