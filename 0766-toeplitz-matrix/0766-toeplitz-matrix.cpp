85f7598041b8ec1a7054649c9115711c59910453
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-10-31 11:24:21 +0530

    Time: 11 ms (98.33%), Space: 17.5 MB (63.25%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -5,16 +5,25 @@ public:
     {
         int m=matrix.size(), n=matrix[0].size();
         
-        if(m==1 || n==1) return true;
+        /*
         int next;
         
         for(int i=0; i<m*n; i++) 
         {
-            if(i%n==n-1) continue;
+            if(i%n==n-1) continue; //for handling rightmost column
             
             next = i+n+1;
             if(next < m*n)
                 if(matrix[i/n][i%n] != matrix[next/n][next%n]) return false;
+        }
+        */
+        
+        for(int i=1; i<m; i++)
+        {
+            for(int j=1; j<n; j++)
+            {
+                if(matrix[i-1][j-1] != matrix[i][j]) return false;
+            }
         }
         return true;
         
