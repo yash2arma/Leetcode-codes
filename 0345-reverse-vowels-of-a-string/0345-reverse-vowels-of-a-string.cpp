783e73bfebfd70150d1449f46c5bf05e43480047
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-11-04 16:29:51 +0530

    Time: 33 ms (6.01%), Space: 8 MB (27.79%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -3,23 +3,15 @@ class Solution
 public:
     string reverseVowels(string s) 
     {
-        vector<char> ds;
+        set<char> ds = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int l=0, r=s.size()-1;
         
-        for(auto it:s)
+        while(l<r)
         {
-            if(it=='a' || it=='e' || it=='i' || it=='o' || it=='u' || it=='A' || it=='E' || it=='I' || it=='O' || it=='U')
-                ds.push_back(it);
-        }
-        
-        int idx=0;
-        for(int i=s.size()-1; i>=0; i--)
-        {
-            char it=s[i];
-            if(it=='a' || it=='e' || it=='i' || it=='o' || it=='u' || it=='A' || it=='E' || it=='I' || it=='O' || it=='U')
-            {
-                s[i]=ds[idx];
-                idx++;
-            }
+            while(l<r && !ds.count(s[l])) l++;
+            while(l<r && !ds.count(s[r])) r--;
+            
+            swap(s[l++],s[r--]);
             
         }
         
