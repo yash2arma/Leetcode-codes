d2115a8c63631a9a36abcdccdff49185f16a92bb
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-11-07 10:02:27 +0530

    Time: 4 ms (19.59%), Space: 6.2 MB (8.65%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,6 +1,7 @@
 class Solution 
 {
 public:
+    /*
     int maximum69Number (int num) 
     {
         string n = to_string(num);
@@ -15,5 +16,34 @@ public:
         }
         
         return stoi(n);
+    }
+    */
+    
+    int maximum69Number (int num) 
+    {
+        int maxi=num;
+        int temp=0, new_n, p=0, rem, x;
+        
+        
+        while(num)
+        {
+            rem = num%10;
+            num /= 10;
+            
+            if(rem==6)
+            {
+                x = pow(10, p)*9 + temp;
+                new_n = num*pow(10, p+1) + x;
+                //cout<<new_n<<" ";
+                maxi = max(maxi, new_n);
+            }
+            
+            temp = (pow(10, p) * rem) + temp;
+            p++;
+            cout<<temp<<" ";
+            
+        }
+        
+        return maxi;
     }
 };
