90caa79a33e8bf6d186352f2dd1f56d9bcf2a825
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-11-09 13:08:07 +0530

    Time: 401 ms (90.61%), Space: 10.3 MB (53.86%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,6 +1,8 @@
 class Solution 
 {
 public:
+    //1. Memoization
+    /*
     int f(int i, int j, vector<int> &a, vector<vector<int>> &dp)
     {
         if(i>j) return 0;
@@ -22,6 +24,33 @@ public:
         a.insert(a.begin(), 1);
         vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
         return f(1, n, a, dp);
+        
+    }
+    */
+    
+    //2. Tabulation
+    int maxCoins(vector<int>& a) 
+    {
+        int n=a.size();
+        a.push_back(1);
+        a.insert(a.begin(), 1);
+        vector<vector<int>> dp(n+2, vector<int> (n+2, 0));
+
+        for(int i=n; i>=1; i--)
+        {
+            for(int j=i; j<=n; j++)
+            {
+                int maxi=INT_MIN;
+                for(int idx=i; idx<=j; idx++)
+                {
+                    int coins = a[i-1]*a[idx]*a[j+1] + dp[i][idx-1] + dp[idx+1][j];
+                    maxi = max(maxi, coins);
+                }
+                dp[i][j] = maxi;
+
+            }
+        }
+        return dp[1][n];
         
     }
 };
