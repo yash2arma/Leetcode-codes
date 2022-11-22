0dc762eb20ac5b0caa13969edbb2fdbcb9df0953
Yash Sharma <63293447+yash2arma@users.noreply.github.com>
2022-11-22 15:16:06 +0530

    Time: 140 ms (85.61%), Space: 9.1 MB (51.59%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -2,6 +2,8 @@ class Solution
 {
 public:
     
+    //1. Memoization
+    /*
     int solve(int n, vector<int> &dp)
     {
         if(n==0) return 0;
@@ -21,5 +23,26 @@ public:
     {
         vector<int> dp(n+1, -1);
         return solve(n, dp);
+    }
+    */
+    
+    //2. Tabulation
+    int numSquares(int n) 
+    {
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        
+        for(int i=1; i<=n; i++)
+        {
+            int minSteps = INT_MAX;
+            for(int j=1; j<=sqrt(i); j++)
+            {
+                int sqr = j*j;
+                int steps = 1 + dp[i-sqr];
+                minSteps = min(minSteps, steps);
+            }
+            dp[i] = minSteps;
+        }
+        return dp[n];
     }
 };
